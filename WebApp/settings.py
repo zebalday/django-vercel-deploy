515407597f5eec8bc5e69464534969d2bdae5ea8
zebalday <sebaalday28@gmail.com>
2024-10-02 15:40:09 -0300

    commit-10

--- WebApp/settings.py
+++ WebApp/settings.py
@@ -171,9 +171,9 @@ USE_TZ = True
 
 # ********************************
 """ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'MainApp/static'),)
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = 'static/' """
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')"""
 
+STATIC_URL = 'static/' 
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
