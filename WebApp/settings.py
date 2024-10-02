79687f8982656c5c368b100fac2db6c5376b6667
zebalday <sebaalday28@gmail.com>
2024-10-01 22:27:09 -0300

    commit-5

--- WebApp/settings.py
+++ WebApp/settings.py
@@ -32,9 +32,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['vercel.app']
+ALLOWED_HOSTS = ['vercel.app', '*']
 
 
 # Application definition
